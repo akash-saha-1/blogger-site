8957b8c65357e488298a4ceb12657b2a6a4c58ec
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-03-25 05:02:26 +0530

    vercel intergration

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -5,63 +5,36 @@ const fs = require("fs");
 
 module.exports = async (req, res) => {
   const { image } = req.files;
-  console.log(image);
   const uploadPath = path.resolve("./../public/posts", image.name);
-  //console.log(uploadPath);
   const deleteFile = (filePath) => {
-    //fs.unlinkSync(filePath);
+    fs.unlinkSync(filePath);
   };
 
-  // image.mv(uploadPath, (err) => {
-  //   if (err) {
-  //     console.log(1);
-  //     console.log(err);
-  //   }
-  //   cloudinary.v2.uploader.upload(image.name, (err, result) => {
-  //     if (err) {
-  //       console.log(2);
-  //       console.log(err);
-  //       deleteFile(uploadPath);
-  //       return res.redirect("/post/new");
-  //     }
+  image.mv(uploadPath, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    cloudinary.v2.uploader.upload(uploadPath, (err, result) => {
+      if (err) {
+        console.log(err);
+        deleteFile(uploadPath);
+        return res.redirect("/post/new");
+      }
 
-  //     Post.create(
-  //       {
-  //         ...req.body,
-  //         image: result.secure_url,
-  //         author: req.session.userId,
-  //       },
-  //       (err, post) => {
-  //         if (err) {
-  //           console.log(3);
-  //           console.log(err);
-  //         }
-  //         deleteFile(uploadPath);
-  //         res.redirect("/");
-  //       }
-  //     );
-  //   });
-  // });
-
-  stream = cloudinary.uploader.upload_stream(
-    function (result) {
-      console.log(result);
-      res.send(
-        'Done:<br/> <img src="' +
-          result.url +
-          '"/><br/>' +
-          cloudinary.image(result.public_id, {
-            format: "png",
-            width: 100,
-            height: 130,
-            crop: "fill",
-          })
+      Post.create(
+        {
+          ...req.body,
+          image: result.secure_url,
+          author: req.session.userId,
+        },
+        (err, post) => {
+          if (err) {
+            console.log(err);
+          }
+          deleteFile(uploadPath);
+          res.redirect("/");
+        }
       );
-    },
-    { public_id: req.body.title }
-  );
-  console.log(req.files.image.path);
-  fs.createReadStream(req.files.image.path, { encoding: "binary" })
-    .on("data", stream.write)
-    .on("end", stream.end);
+    });
+  });
 };
