d0bd72bafb9c86e011e27467fa0a74ec58db72d3
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-03-25 03:59:01 +0530

    restored

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -12,55 +12,55 @@ module.exports = async (req, res) => {
     //fs.unlinkSync(filePath);
   };
 
-  // image.mv(uploadPath, (err) => {
-  //   if (err) {
-  //     console.log(1);
-  //     console.log(err);
-  //   }
-  // cloudinary.v2.uploader.upload(image.name, (err, result) => {
-  //   if (err) {
-  //     console.log(2);
-  //     console.log(err);
-  //     deleteFile(uploadPath);
-  //     return res.redirect("/post/new");
-  //   }
+  image.mv(uploadPath, (err) => {
+    if (err) {
+      console.log(1);
+      console.log(err);
+    }
+    cloudinary.v2.uploader.upload(image.name, (err, result) => {
+      if (err) {
+        console.log(2);
+        console.log(err);
+        deleteFile(uploadPath);
+        return res.redirect("/post/new");
+      }
 
-  //   Post.create(
-  //     {
-  //       ...req.body,
-  //       image: result.secure_url,
-  //       author: req.session.userId,
-  //     },
-  //     (err, post) => {
-  //       if (err) {
-  //         console.log(3);
-  //         console.log(err);
-  //       }
-  //       deleteFile(uploadPath);
-  //       res.redirect("/");
-  //     }
-  //   );
-  // });
-  //});
-  // stream = cloudinary.uploader.upload_stream(
-  //   function (result) {
-  //     console.log(result);
-  //     res.send(
-  //       'Done:<br/> <img src="' +
-  //         result.url +
-  //         '"/><br/>' +
-  //         cloudinary.image(result.public_id, {
-  //           format: "png",
-  //           width: 100,
-  //           height: 130,
-  //           crop: "fill",
-  //         })
-  //     );
-  //   },
-  //   { public_id: req.body.title }
-  // );
-  // console.log(req.files.image.path);
-  // fs.createReadStream(req.files.image.path, { encoding: "binary" })
-  //   .on("data", stream.write)
-  //   .on("end", stream.end);
+      Post.create(
+        {
+          ...req.body,
+          image: result.secure_url,
+          author: req.session.userId,
+        },
+        (err, post) => {
+          if (err) {
+            console.log(3);
+            console.log(err);
+          }
+          deleteFile(uploadPath);
+          res.redirect("/");
+        }
+      );
+    });
+  });
+  stream = cloudinary.uploader.upload_stream(
+    function (result) {
+      console.log(result);
+      res.send(
+        'Done:<br/> <img src="' +
+          result.url +
+          '"/><br/>' +
+          cloudinary.image(result.public_id, {
+            format: "png",
+            width: 100,
+            height: 130,
+            crop: "fill",
+          })
+      );
+    },
+    { public_id: req.body.title }
+  );
+  console.log(req.files.image.path);
+  fs.createReadStream(req.files.image.path, { encoding: "binary" })
+    .on("data", stream.write)
+    .on("end", stream.end);
 };
